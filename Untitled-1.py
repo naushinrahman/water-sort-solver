dac5babe1a51b594333815be3efbef27232f868b
Naushin Rahman <naushin.r@gmail.com>
2024-10-14 15:08:45 -0400

    adding more functions

--- Untitled-1.py
+++ Untitled-1.py
@@ -5,7 +5,6 @@
 num_of_bottles = 5
 
 # ex
-
 # blue   yellow   blue   empty   empty
 # blue   yellow    red   empty   empty
 # yellow  red     red    empty   empty
@@ -29,28 +28,23 @@ class Bottle:
         if self.isEmpty() is False:
             self.top-=1
             self.used-=1
-    
+
+
     def isEmpty(self):
         return True if self.used == 0 and self.top == -1 else False
     
     def isFull(self):
         return True if self.used == self.size and self.used > 0 else False
-    
-    def inStack(self, colour):
+
+
+    def search(self, colour):
         instances_of_colour = []
         for item in self.items:
-            if item.lower() == colour.lower():
+            if item.lower() == colour.lower():  #want to be case insensitive 
                 index = self.items.index(colour)
                 instances_of_colour.append(index)
 
         return False if len(instances_of_colour) > 0 else instances_of_colour
-                
-    
-    def pour(self):
-        pass
-    
-    def search(self, colour):
-        pass
     
     def isSorted(self):
         top = self.items[self.top]
@@ -60,8 +54,38 @@ class Bottle:
                 flag = False
         
         return True if flag == True else False
-            
     
+    def emptySpaces(self):
+        counter = 0
+           
+        for item in self.items:
+            if item == 'e':
+                counter+=1   
+                     
+        if self.isEmpty() == True:    #should i check isEmpty or isFull here or in pour f'n?
+            counter = 4
+        if self.isFull() == True:
+            counter = 0
+        
+        return counter    #feel like im missing something here
+            
+
+def pour(initial_bottle, final_bottle):
+    '''
+    need to search in initial bottle
+    	#if false return error message
+    else count length of array returned
+    then check emptySpaces of final bottle
+		#if there are no empty spaces in final bottle return error message
+	if there are empty spaces check top of both bottles
+		#if tops match: 
+			count how much of the same colour
+   			increment final.used in a loop until either 
+    
+    '''
+    pass
+	
+
+
 def user_input():
     pass
-
