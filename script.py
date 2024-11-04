e29b87473a62bc4476ebac19131b58666093780c
Naushin Rahman <naushin.r@gmail.com>
2024-11-03 21:30:01 -0500

    pour function completed and tested

--- script.py
+++ script.py
@@ -14,47 +14,59 @@ class Bottle:
         self.top_colour = None
     
     def push(self, colour):
-        if self.isFull():
-            print("Error: Cannot add: Bottle is full")
+        if self.is_full():
+            print("Cannot add: Bottle is full")
             return
-        self.top+=1
-        self.items[self.top] = colour
-        if colour != 'empty':
-            self.used+=1
+        if colour != 'empty':      #when it is a colour
             self.top_colour = colour
+            self.used+=1
+            self.top+=1
+            self.items[self.top] = colour
     
     def pop(self):
-        if self.isEmpty():
-            print("Error: Cannot remove: Bottle is empty")
+        if self.is_empty():
+            print("Cannot remove: Bottle is empty")
             return
-        if self.top_colour == 'empty':
-            print("Error: Cannot pop, top is empty")
-            return
-        self.top_colour = 'empty'
+        self.items[self.top] = None
         self.top-=1
         self.used-=1
-        self.top_colour = self.items[self.top] if self.top >= 0 else 'empty'
+        self.top_colour = self.items[self.top] if self.top >= 0 else None
 
 
-    def isEmpty(self):
+    def is_empty(self):
         return True if self.used == 0 and self.top == -1 else False
     
-    def isFull(self):
+    def is_full(self):
         return True if self.used == self.size and self.used > 0 else False
     
     def print(self):
-        for i in range(self.top, -1, -1):
+        for i in range((self.size-1), -1, -1):
             print(self.items[i])
 
-    def search(self, colour):
+    def search(self, colour):       #returns array of top colour/emptyspaces
         instances_of_colour = []
-        for i in range(self.top, -1, -1):
-            if self.items[i] == colour:               
+        
+        for i in range((self.used-1), -1, -1):
+            if self.items[i] == colour:
                 instances_of_colour.append(colour)
+            else:               
+                break
         return False if len(instances_of_colour) <= 0 else instances_of_colour
     
+    def search_empty(self, colour):       #returns array of top colour/emptyspaces
+        instances_of_colour = []
+        
+        for i in range((self.size-1), -1, -1):
+            if self.items[i] == colour:
+                instances_of_colour.append(colour)
+            else:               
+                break
+        return False if len(instances_of_colour) <= 0 else instances_of_colour
     
-    def isSorted(self):
+    def return_top_colour(self):
+        return self.top_colour
+    
+    def is_sorted(self):
         top = self.items[self.top]
         flag = True
         for item in self.items:
@@ -63,63 +75,55 @@ class Bottle:
         return True if flag == True else False
     
     
-    def emptySpaces(self):
+    def empty_spaces(self):
         counter = 0
            
         for item in self.items:
             if item == 'e':
                 counter+=1   
                      
-        if self.isEmpty() == True:    #should i check isEmpty or isFull here or in pour f'n?
+        if self.is_empty() == True:    #should i check isEmpty or isFull here or in pour f'n?
             counter = 4
-        if self.isFull() == True:
+        if self.is_full() == True:
             counter = 0
         return counter    #feel like im missing something here
             
 
 def pour(initial_bottle, final_bottle):
     
-    if initial_bottle.isEmpty():            #check if initial bottle is empty
+    if initial_bottle.is_empty():            #check if initial bottle is empty
         print('Error: Nothing in initial bottle, cannot pour')
         return
-	    
-    top_colour = initial_bottle.top_colour  
-    num_colour = len(initial_bottle.search(top_colour))
     
-    try:
-        num_of_spaces = len(final_bottle.search('empty'))
-    except TypeError:
+    if final_bottle.is_full():            
         print('Error: Bottle full, cannot pour')
-    
-    matching_top = final_bottle.search(top_colour)
-    if matching_top != top_colour:
-        print('Error: cannot pour on to a different colour')
         return
     
-    while num_of_spaces > 0:
-        
-        final_bottle.used+=1
-        initial_bottle.used-=1
-        num_colour-=1
-        num_of_spaces-=1
+    top_colour = initial_bottle.return_top_colour()
+    num_colour = len(initial_bottle.search(top_colour))
     
+    if final_bottle.is_empty() is False:
+        matching_top = final_bottle.return_top_colour()
+        if matching_top != top_colour:
+            print('Error: cannot pour on to a different colour')
+            return
     
-'''
-need to search in initial bottle
-	#if false return error message
-else count length of array returned
-then check emptySpaces of final bottle in a var
-	#if there are no empty spaces in final bottle return error message
-if there are empty spaces check top of both bottles
-	#if tops match: 
-		count how much of the same colour in initial bottle in a var
-		while emptySpaces > 0: final.used+=1, initial.used-=1, emptySpaces-=1 
-'''
+    num_of_spaces = len(final_bottle.search_empty(None))
+    
+    try:
+        while num_of_spaces > 0 and num_colour > 0:
+            final_bottle.push(top_colour)
+            initial_bottle.pop()
+            num_colour-=1
+            num_of_spaces-=1
+    except:
+        print("Error: pour unsuccessful")
+    
 
 def sort():
     pass
 
-def user_input():                      #add documentation for this function
+def user_input():                    
 	global array_of_bottles
 	count_of_bottles = num_of_bottles
 	current_index = 0
@@ -129,19 +133,29 @@ def user_input():                      #add documentation for this function
 		
 		counter = 1
 		temp = Bottle()
+		is_valid = True
+		previous_was_empty = False
 
 		while counter <= 4:
-			print('Enter colour number ', counter, '(from bottom to top)')
-			colour = input(' : ')  #need to check: emptySpaces can only have other emptySpaces on top
-								   #and that im taking lower case input only
+			colour = input(f'Enter colour number {counter} (from bottom to top): ').strip().lower()
+
+			if colour == "" or colour == "empty":
+				if not previous_was_empty:
+					previous_was_empty = True
+				else:
+					print("Error: Empty spaces can only be on top of other empty spaces.")
+					is_valid = False
+					break
+			else:
+				previous_was_empty = False
+
 			temp.push(colour)
 			counter+=1
 
-		array_of_bottles[current_index] = temp
-
-		current_index+=1
-		count_of_bottles-=1
-
+		if is_valid:
+			array_of_bottles[current_index] = temp
+			current_index+=1
+			count_of_bottles-=1
 
 # ex
 # blue   yellow   blue   empty   empty
@@ -149,14 +163,23 @@ def user_input():                      #add documentation for this function
 # yellow  red     red    empty   empty
 # yellow  blue    red    empty   empty
 
-
 user_input()
 
 for item in array_of_bottles:  
+		print('\n')
 		item.print()
-		print('\n')
 		print('top colour: ', item.top_colour)
-		print('\n')
+		print('index:', item.top)
+		print('used:', item.used)
 
 
 pour(array_of_bottles[0], array_of_bottles[1])
+
+print('after pour:')
+
+for item in array_of_bottles:  
+		print('\n')
+		item.print()
+		print('top colour: ', item.top_colour)		
+		print('index:', item.top)
+		print('used:', item.used)
